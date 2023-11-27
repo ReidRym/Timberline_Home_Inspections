5c7339ed95d0d5493a974c49d6307cbfcf2aedd6
Ryan R <irishreid55@gmail.com>
2023-11-26 17:44:03 -0800

    added css and media queries

--- Info_Header/infoHeader.js
+++ Info_Header/infoHeader.js
@@ -1,10 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.getElementById('toggleMenu');
-  const navList = document.querySelector('.header-main-nav');
+$(document).ready(function() {
+    $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        e.stopPropagation();
+    });
 
-  toggleButton.addEventListener('click', function () {
-    navList.classList.toggle('show');
-  });
+    $('html').click(function(){
+        $('.nav-dropdown').hide();
+    })
+    $('#nav-toggle').click(function(){
+        $('nav ul').slideToggle();
+    })
+    $('#nav-toggle').on('click', function(){
+        this.classList.toggle('active');
+    });
 });
-
-
