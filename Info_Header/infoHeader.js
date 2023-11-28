ef4f65c0aae0ec4c94d371c01bca1581af3c5ea6
Ryan R <irishreid55@gmail.com>
2023-11-27 21:40:41 -0800

    added java for navbar hamburger

--- Info_Header/infoHeader.js
+++ Info_Header/infoHeader.js
@@ -1,16 +1,30 @@
-$(document).ready(function() {
-    $('nav ul li a:not(:only-child)').click(function(e) {
-        $(this).siblings('.nav-dropdown').toggle();
-        e.stopPropagation();
-    });
+$(document).ready(function () {
+  $("nav ul li a:not(:only-child)").click(function (e) {
+    $(this).siblings(".nav-dropdown").toggle();
+    e.stopPropagation();
+  });
 
-    $('html').click(function(){
-        $('.nav-dropdown').hide();
-    })
-    $('#nav-toggle').click(function(){
-        $('nav ul').slideToggle();
-    })
-    $('#nav-toggle').on('click', function(){
-        this.classList.toggle('active');
-    });
+  $("html").click(function () {
+    $(".nav-dropdown").hide();
+  });
+  $("#nav-toggle").click(function () {
+    $("nav ul").slideToggle();
+  });
+  $("#nav-toggle").on("click", function () {
+    this.classList.toggle("active");
+  });
+});
+// infoHeader.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.getElementById('nav-toggle');
+  const mainQuotes = document.querySelector('.main-quotes');
+  const mainIntro = document.querySelector('.main-intro');
+
+  navToggle.addEventListener('click', function () {
+    // Toggle the class when the hamburger icon is clicked
+    mainQuotes.classList.toggle('hide-on-mobile');
+    mainIntro.classList.toggle('hide-on-mobile');
+  });
+  
 });
